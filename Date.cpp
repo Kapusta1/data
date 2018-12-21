59c9111aa7e7d9eb5bc17aed70d28869feb64b86
tom <tomestrader@gmail.com>
2018-12-22 00:41:36 +0300

    add operator Date < Date , Date <= Date, Date > Date, Date >= Date, and -=, +=

--- Date.cpp
+++ Date.cpp
@@ -17,16 +17,8 @@ private:
 public:
 	Date(int InputDay, int InputMonth, int InputYear) :
 		Day(InputDay), Month(InputMonth), Year(InputYear) {};
-	Date & operator++()
-	{
-		++Day;
-		return *this;
-	}
-	Date& operator--()
-	{
-		--Day;
-		return *this;
-	}
+	Date & operator++(){++Day; return *this;}
+	Date & operator--(){--Day; return *this;}
 
 	Date operator++(int)
 	{
@@ -60,6 +52,49 @@ public:
 		return Date(Day - daysToSub, Month, Year);
 	}
 
+	void operator +=(int DaysToAdd){Day += DaysToAdd;}
+	void operator -=(int DaysToAdd) { Day -= DaysToAdd; }
+
+	bool operator == (const Date & compareTo)
+	{
+		return ((Day == compareTo.Day) && (Month == compareTo.Month) && (Year == compareTo.Year));
+	}
+	bool operator != (const Date & compareTo)
+	{
+		return !(this->operator==(compareTo));
+	}
+
+	bool operator < (const Date & compareTo)
+	{
+		if (Day < compareTo.Day)
+			return true;
+		else
+			if (Month < compareTo.Month)
+				return true;
+			else
+				if (Year < compareTo.Year)
+					return true;
+				else
+					return false;
+	}
+	bool operator <= (const Date & compareTo)
+	{
+		if (this->operator==(compareTo))
+			return true;
+		else
+			return this->operator<(compareTo);
+	}
+	bool operator > (const Date & compareTo)
+	{
+		return !(this->operator<(compareTo));
+	}
+	bool operator >= (const Date & compareTo)
+	{
+		if (this->operator==(compareTo))
+			return true;
+		else
+			return this->operator>(compareTo);
+	}
 	void DisplayDate()
 	{
 		cout << Day << " / " << Month << " / " << Year << endl;
@@ -68,18 +103,26 @@ public:
 
 int main()
 {
-	Date Holiday(25, 11, 2011);
+	Date Holiday1(25, 11, 2011);
+	Date Holiday2(31, 11, 2011);
 
-	cout << "Holliday on: ";
-	Holiday.DisplayDate();
+	cout << "Holiday 1 is: ";
+	Holiday1.DisplayDate();
+	cout << "Holiday 2 is: ";
+	Holiday2.DisplayDate();
 
-	Date PreviousHoliday(Holiday - 19);
-	cout << "Previous holiday on: ";
-	PreviousHoliday.DisplayDate();
+	if (Holiday1 < Holiday2)
+		cout << "operator < : Holliday1 happens first" << endl;
+	
 
-	Date NextHoliday(Holiday + 6);
-	cout << "Next holiday on: ";
-	NextHoliday.DisplayDate();
+	if(Holiday1>Holiday2)
+		cout << "operator > : Holliday2 happens later" << endl;
+
+	if(Holiday1<=Holiday2)
+		cout << "operator <= : Holliday1 happens on or before Holliday2" << endl;
+
+	if (Holiday2 <= Holiday1)
+		cout << "operator >= : Holliday2 happens on or before Holliday1" << endl;
 
 	return 0;
 }
